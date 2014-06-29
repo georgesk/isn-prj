3b86801e83e4cb54fd38de2ab4a67c406f7770c6
georgesk <georgesk@debian.org>
2014-06-29 19:48:03 +0200

    corrigé un bug pour le mouvement des curseurs quand le tableau est mis à jour

--- sub/eval.js
+++ sub/eval.js
@@ -21,94 +21,17 @@
 
 /**
  * le programme suppose que les bibliothèques jquery et jquery-ui sont
- * chargées par avance.
+ * chargées par avance, ainsi que les bibliothèques 
+ * competences.js et indicateurs.js
  **/
-
-var competences ={
-    "C1":{
-	"desc":"Décrire et expliquer une situation, un système ou un programme",
-	"sub":{
-	    "C1.1":{
-		"desc":"Justifier/dans une situation donnée, un codage numérique ou l'usage d'un format approprié, qu'un programme réalise l'action attendue...",
-		"targets":[1,]
-	    },
-	    "C1.2":{
-		"desc":"Détailler/le déroulement d'une communication numérique, le rôle des constituants d'un système numérique, le rôle des éléments constitutifs d'une page web, ce qu'effectue tout ou partie d'un programme ou de l'algorithme associé, l'enchaînement des événements qui réalisent la fonction attendue par un programme...",
-		"targets":[0,1,]	
-	    },
-	},
-    },
-    "C2":{
-	"desc":"Concevoir et réaliser une solution informatique en réponse à un problème",
-	"sub":{
-	    "C2.1":{
-		"desc":"Analyser/un besoin dans un système d'information, le fonctionnement d'un algorithme … ",
-		"targets":[0,1,]	
-	    },
-	    "C2.2":{
-		"desc":" Structurer/une formule logique, des données, une arborescence, une page web, une approche fonctionnelle en réponse à un besoin…",
-		"targets":[0,1,]	
-	    },
-	    "C2.3":{
-		"desc":"Développer/une interface logicielle ou une interface homme-machine, un algorithme, un programme, un document ou fichier numérique…",
-		"targets":[0,1,]	
-	    },
-	},
-    },
-    "C3":{
-	"desc":"Collaborer efficacement au sein d'une équipe dans le cadre d'un projet",
-	"sub":{
-	    "C3.1":{
-		"desc":"Agir au sein d'une équipe/dans des rôles bien définis, en interaction avec le professeur.",
-		"targets":[0,1,]	
-	    },
-	    "C3.3":{
-		"desc":"Maîtriser l'utilisation d'outils numériques collaboratifs/du type ENT, système de gestion de contenu (CMS), groupe de travail, forums...",
-		"targets":[1,]	
-	    },
-	},
-    },
-    "C4":{
-	"desc":"Communiquer à l'écrit et à l'oral",
-	"sub":{
-	    "C4.1":{
-		"desc":"Documenter un projet numérique/pour en permettre la communication en cours de réalisation et à l'achèvement, tout en précisant le déroulement et la finalité du projet.",
-		"targets":[0,]	
-	    },
-	    "C4.2":{
-		"desc":"Présenter/le cahier des charges relatif à un projet ou un mini-projet, la répartition des tâches au sein de l'équipe, les phases successives mises en œuvre, le déroulement de l'ensemble des opérations…",
-		"targets":[0,1,]	
-	    },
-	    "C4.3":{
-		"desc":"Argumenter/les choix relatifs à une solution (choix d'un format, d'un algorithme, d'une interface…)",
-		"targets":[0,1,]	
-	    },
-	},
-    },
-    "Globalisation":{
-	"desc":"Cette partie doit permettre une prise en compte des éléments saillants apparus lors de la présentation et du dialogue : culture, réactivité, questionnements éthiques, etc.",
-	"targets":[0,1]
-    }
-}
-
-/**
- * variables globales concernant les capacités et les indicateurs, les scores
- **/
-var indicateurs = [["A expliqué le codage de l'envoi de courriel","C1.1"],
-		   ["A détaillé la transmission du courriel","C1.2"],
-		   ["A créé la page web permettant de consulter une alerte","C2.2"],
-		   ["S'est chargé de la gestion de version du code avec Git","C3.3"], 
-		   ["A argumenté le choix de la bibliothèque OpenCV","C4.3"],
-		   ["A bien délimité la protection de la vie privée de l'utilisateur","Globalisation"],
-		  ];
-
-var nbCapacites = indicateurs.length; // nombre d'indicateurs
-var levels = [0, 1, 2, 3]; // niveaux possibles
 
 
 /**
  * variables globales concernant la disposition des curseurs
  **/
+var nbCapacites = indicateurs.length; // nombre d'indicateurs
+var levels = [0, 1, 2, 3]; // niveaux possibles
+
 var r1 = 6, r2 = 9;        // plus petite et plus grande valeur du rayon
 var gliders=[];            // liste des curseurs
 
@@ -163,10 +86,8 @@ function ajusteIndicateurTableau(index, val){
  **/
 function ajusteIndicateurGraphique(index, val){
     var g=gliders[index];
-    // peut être un bug de jsxgraph 0.98 ?
-    // setGliderPosition ne fonctionne pas à la toute première invocation
-    g.setGliderPosition(val/(levels.length-1));
-    g.setGliderPosition(val/(levels.length-1));
+    var r=r1+(r2-r1)*val/(levels.length-1);
+    g.moveTo([Math.cos(g.angle)*r, Math.sin(g.angle)*r],1000)
 }
 
 /**
